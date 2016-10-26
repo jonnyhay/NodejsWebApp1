8fdffdfa3f72356d2dba1e380dc86ea9f455ed96
Jonny <Jonny@JONNYLENOVO2015>
2016-10-25 22:08:59 -0700

    Adding better logging with date time info

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -4,22 +4,48 @@ var fs = require('fs');
 var os = require("os");
 var port = process.env.port || 1337;
 
-var choices = ["testreply1", "testreply2", "testreply3", "testreply4", "testreply5"];
+var choices = ["CreditScore800", "CreditScore720", "CreditScore560", "CreditScore420", "CreditScore305"];
+
+function getParameterByName(name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+// For todays date;
+Date.prototype.today = function () {
+    return ((this.getDate() < 10) ? "0" : "") + this.getDate() + "/" + (((this.getMonth() + 1) < 10) ? "0" : "") + (this.getMonth() + 1) + "/" + this.getFullYear();
+}
+
+// For the time now
+Date.prototype.timeNow = function () {
+    return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
+}
 
 http.createServer(function (req, res) {
 
     var path = url.parse(req.url).pathname;
-
+    console.log(req.url);
+    console.log(path);
     if (path == "/GetCustomerData") {
-        console.log("ajax req received");
+        
+        customerInfo = getParameterByName('cusInfo', req.url);
+        console.log("ajax req received: " + customerInfo);
+
         var resString = choices[Math.floor(Math.random() * choices.length)];
         console.log("string '" + resString + "' chosen");
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end(resString);
         console.log("string sent");
-
+        var datetime = new Date().today() + " @ " + new Date().timeNow();
         // log 
-        fs.appendFile("./log_requests.txt", req.ip + "," + req.url + "," + resString + os.EOL, function (err) {
+        fs.appendFile("./log_requests.txt", req.ip + "," + datetime + "," + resString + "," + customerInfo + os.EOL, function (err) {
         if (err) {
             return console.log(err);
         }
